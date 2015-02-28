b2bdad6327b857d6d4cd6d1b81dad149499276e2
Benton Li <bentonli@comcast.net>
2015-02-28 11:53:53 -0800

    started parser

--- src/csvParser.java
+++ src/csvParser.java
@@ -1,4 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class csvParser {
+	
+	public static void main(String args[]) {
+		csvParser csvP = new csvParser();
+		csvP.run();
+	}
+	
+	public void run() {
+		
+		String csvFile = "";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = "|";
+		
+		
+		
+		
+	}
 
 }
