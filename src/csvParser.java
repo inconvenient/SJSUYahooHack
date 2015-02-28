9049f4d6126ead8c1ef0ed32e2acc88bbbe1ce95
Benton Li <bentonli@comcast.net>
2015-02-28 12:06:56 -0800

    starting parser

--- src/csvParser.java
+++ src/csvParser.java
@@ -17,8 +17,20 @@ public class csvParser {
 		String line = "";
 		String cvsSplitBy = "|";
 		
+		// Severity|Count|Node|Property|First Occurrence|Summary
 		
-		
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+			
+			while ((line = br.readLine()) != null) {
+				String[] Error = line.split(cvsSplitBy); // Split by |
+	 
+				System.out.println("Error [Severity= " + Error[0] 
+	                                 + " , Count=" + Error[1] + " , Node=" + Error[2] 
+	                                	+ " , Property=" + Error[3] + " , First Occur=" 
+	                                	   + Error[4] + "Summary" + Error[5] + "]");
+	 
+			}
 		
 	}
 
